1be3ee25cea4af9a197e338d7dacc8470409505f
FMCalisto <francisco_mcalisto@hotmail.com>
2014-10-31 23:51:19 +0000

    Update
    Update new <restricoes.lisp> file.

---
+++ src/restricao.lisp
@@ -0,0 +1,53 @@
+; GRUPO 17
+;
+; Bruno Oliveira
+; 67371
+;
+; Daniel da Costa
+; 69720
+;
+; Francisco Maria Calisto
+; 70916
+
+; ======================================================================================== ;
+;                                         Tipo Restricao                                   ;
+; ======================================================================================== ;
+
+; O tipo Restricao e utilizado para representar uma restricao de um PSR
+
+
+; LOAD
+
+; Chamada dos Ficheiros
+
+(load "aux-func.lisp")
+
+
+; CRIA RESTRICAO
+
+; Este construtor recebe uma lista das variaveis envolvidas na restricao,
+; e uma funcao que verifica a restricao.
+
+(defun cria-restricao (lst pred)
+
+	)
+
+
+; RESTRICAO VARIAVEIS
+
+; Este selector, dada uma restricao r,
+; retorna uma lista com todos as variaveis envolvidas na restricao.
+
+(defun restricao-variaveis (r)
+
+	)
+
+
+; RESTRICAO FUNCAO VALIDACAO
+
+; Este selector, dada uma restricao r,
+; retorna a funcao de avaliacao usada para verificar se a restricao e satisfeita.
+
+(defun restricao-funcao-validacao (r)
+
+	)
